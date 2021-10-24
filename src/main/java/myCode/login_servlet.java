c67a065d8ada02eac6c7ed28ab8d901fee7a325c
sairamanan1810 <mksroct2000@gmail.com>
2021-10-24 19:25:31 +0530

    Servlet

--- src/main/java/myCode/login_servlet.java
+++ src/main/java/myCode/login_servlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.*;
 import java.util.*;
 import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.xdevapi.Result;
 /**
  * Servlet implementation class login_servlet
  */
@@ -42,17 +43,51 @@ public class login_servlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		PrintWriter out=response.getWriter();
 		response.setContentType("text/html");
-		String roll_no=request.getParameter("roll_no");
+		String name=request.getParameter("name");
 		String password=request.getParameter("pwd");
 		System.out.println("In Post");
 		HttpSession session=request.getSession();
-		session.setAttribute("id",roll_no);
 		
+		String sql="Select * from logincredentials where username=? and password=?";
+		try {
+		Connection con = JDBC_connection.initializedatabase();
+		
+        // Create a SQL query to insert data into demo table
+        // demo table consists of two columns, so two '?' is used
+        PreparedStatement st = con.prepareStatement(sql);
 
-		System.out.println(roll_no);
+        // For the first parameter,
+        // get the data using request object
+        // sets the data to st pointer
+        st.setString(1, name);
+
+        // Same for second parameter
+        st.setString(2, password);
+        // Execute the insert command using executeUpdate()
+        // to make changes in database
+        ResultSet rs = st.executeQuery();
+        
+        while(rs.next()) {
+	        
+	        if(rs.getString(2).equals(name) && rs.getString(3).equals(password)) {
+	        	session.setAttribute("id",name);
+	        	request.getRequestDispatcher("/DashBoard.html").forward(request, response);
+	        	st.close();
+	            con.close();
+	        }	
+	        }
+	        request.getRequestDispatcher("/login.html").include(request, response);
+	        // Close all the connections
+        
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(name);
 		System.out.println(password);
 
 	}
